d85100da61c0ac16131fc048cb934f913819b924
Yashasvi <yashasvivij01@gmail.com>
2023-08-11 21:32:33 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const App = () => {
+  const [toggle, setToggle] = React.useState(true);
+
+  function handleToggle() {
+    let div1 = document.getElementById("welcome-div");
+    let div2 = document.getElementById("about-div");
+    if (toggle) {
+      div1.style.display = "block";
+      div2.style.display = "none";
+      setToggle(false);
+    } else {
+      div1.style.display = "none";
+      div2.style.display = "block";
+      setToggle(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div id="welcome-div">
+        <h1>Welcome to Newton School!!</h1>
+      </div>
+      <div id="about-div">
+        <h1>
+          Newton School is a neo-university providing highly immersive tech
+          learning to millions of students, to tap into new-age tech
+          opportunities.
+        </h1>
+      </div>
+      <button id="toggle" onClick={handleToggle}>
+        Toggle
+      </button>
     </div>
   );
-}
+};
 
 export default App;
